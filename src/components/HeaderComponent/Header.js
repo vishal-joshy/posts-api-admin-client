22d5b01333bedf8d714f4f2e2c08a62eb3f2fe75
VishalJoshy <1812-vishal@protonmail.com>
2021-10-02 20:15:52 +0530

    Initial commit

---
+++ src/components/HeaderComponent/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+
+function Header() {
+	return (
+		<div>
+			<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+				<Container>
+					<Navbar.Brand href='#home'>Posts</Navbar.Brand>
+					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+					<Navbar.Collapse id='responsive-navbar-nav'>
+						<Nav className='me-auto'></Nav>
+						<Nav>
+							<Nav.Link href='#deets'>Sign In</Nav.Link>
+							<Nav.Link href='#memes'>Sign Up</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</div>
+	);
+}
+
+export default Header;
